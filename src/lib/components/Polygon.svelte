d473c98a5a1f93834f8a418b88b09ef550d46ba3
Naratna <antar.espadas@hotmail.com>
2024-06-25 03:31:36 -0600

    Initial commit

---
+++ src/lib/components/Polygon.svelte
@@ -0,0 +1,151 @@
+<script lang="ts">
+	import { onMount, createEventDispatcher } from 'svelte';
+
+	export let editing = false;
+	export let id: string;
+	export let gridX: number;
+	export let gridY: number;
+	export let editMode = false;
+	export let disabled = false;
+
+	const dispatch = createEventDispatcher<{ closed: {} }>();
+
+	let points: Point[] = [];
+	let dragTarget: Point | undefined = undefined;
+	let closed = false;
+
+	$: opacity = editMode ? 1 : 0;
+
+	$: svgPoints = getSvgPoints(points, closed, gridX, gridY);
+
+	$: if (!editing) {
+		document.removeEventListener('mousemove', onDrag);
+		dragTarget = undefined;
+	}
+
+	onMount(() => {
+		document.addEventListener('mousemove', onDrag);
+		document.addEventListener('click', addPoint);
+		document.addEventListener('mouseup', mouseUp);
+
+		return () => {
+			document.removeEventListener('mousemove', onDrag);
+			document.removeEventListener('click', addPoint);
+			document.removeEventListener('mouseup', mouseUp);
+		};
+	});
+
+	function getSvgPoints(points: Point[], closed: boolean, girdX: number, gridY: number) {
+		let pointsStr = 'M' + points.map((p) => p.x * gridX + ' ' + p.y * gridY).join(' L ');
+		if (closed) pointsStr += ' Z';
+		return pointsStr;
+	}
+
+	function snap(x: number, y: number, gridX: number, gridY: number): [number, number] {
+		const lowestX = Math.floor(x / gridX) * gridX;
+		const lowestY = Math.floor(y / gridY) * gridY;
+		const xDiff = x - lowestX;
+		const yDiff = y - lowestY;
+
+		let targetX = lowestX;
+		if (xDiff > gridX / 2) {
+			targetX += gridX;
+		}
+
+		let targetY = lowestY;
+		if (yDiff > gridY / 2) {
+			targetY += gridY;
+		}
+
+		return [targetX / gridX, targetY / gridY];
+	}
+
+	function addPoint(e: MouseEvent) {
+		if (!editing) return;
+		if (closed) return;
+		const [x, y] = snap(e.clientX, e.clientY, gridX, gridY);
+		points.push({
+			x,
+			y,
+			first: points.length == 0
+		});
+		points = points;
+	}
+
+	function pointClicked(point: Point) {
+		if (!editing) return;
+		if (point.dragged) {
+			delete point.dragged;
+			return;
+		}
+		if (point.first) {
+			closed = true;
+			dispatch('closed');
+		}
+	}
+
+	function pointMouseDown(point: Point) {
+		if (!editing) return;
+		dragTarget = point;
+	}
+
+	function mouseUp() {
+		if (!editing) return;
+		dragTarget = undefined;
+	}
+
+	async function onDrag(e: MouseEvent) {
+		if (!editing) return;
+		if (dragTarget === undefined) return;
+		const [x, y] = snap(e.clientX, e.clientY, gridX, gridY);
+		dragTarget.x = x;
+		dragTarget.y = y;
+		// We can later use this to skip over 'click' events, as they should not be fired if the element was dragged
+		dragTarget.dragged = true;
+		points = points;
+	}
+
+	interface Point {
+		x: number;
+		y: number;
+		first: boolean;
+		dragged?: boolean;
+	}
+
+	interface Polygon {
+		points: Point[];
+	}
+</script>
+
+<path
+	d={svgPoints}
+	style:fill={closed ? 'rgba(0, 247, 247, 0.25)' : 'none'}
+	style="
+	--opacity:{opacity};
+	--pointer-events:{disabled ? 'none' : 'all'}"
+/>
+{#if editing}
+	{#each points as point}
+		<circle
+			cx={point.x * gridX}
+			cy={point.y * gridY}
+			r="10"
+			fill="rgb(17, 155, 155)"
+			on:click|stopPropagation={() => pointClicked(point)}
+			on:mousedown|stopPropagation={() => pointMouseDown(point)}
+		/>
+	{/each}
+{/if}
+
+<style>
+	path {
+		opacity: var(--opacity);
+		stroke: rgba(0, 122, 204, 0.5);
+		stroke-width: 3px;
+		pointer-events: var(--pointer-events);
+	}
+
+	path:hover {
+		opacity: 1;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import Polygon from '$lib/components/Polygon.svelte';
+
+	let polygons: string[] = [];
+	let activePolygon: string | undefined = undefined;
+	let buttonLabel: 'add' | 'cancel' | 'done' = 'add';
+	let gridX = 61.5;
+	let gridY = 61.5;
+	let showGrid = false;
+	$: console.log(showGrid);
+
+	function buttonClicked() {
+		if (buttonLabel === 'add') {
+			const id = crypto.randomUUID();
+			polygons.push(id);
+			polygons = polygons;
+			activePolygon = id;
+			buttonLabel = 'cancel';
+		} else if (buttonLabel === 'cancel') {
+			polygons = polygons.filter((p) => p !== activePolygon);
+			activePolygon = undefined;
+			buttonLabel = 'add';
+		} else if (buttonLabel === 'done') {
+			activePolygon = undefined;
+			buttonLabel = 'add';
+		}
+	}
+
+	function polygonClosed() {
+		buttonLabel = 'done';
+	}
+</script>
+
+<div class="map">
+	{#if showGrid}
+		<div class="grid" style="--grid-x:{gridX}px; --grid-y:{gridY}px"></div>
+	{/if}
+	<div class="controls">
+		<button class="add-button" on:click|stopPropagation={buttonClicked}>{buttonLabel}</button>
+		<label for="grid-x">Grid X</label>
+		<input id="grid-x" type="number" bind:value={gridX} />
+		<label for="grid-y">Grid Y</label>
+		<input id="grid-y" type="number" bind:value={gridY} />
+		<label for="show-grid">Grid Y</label>
+		<input id="show-grid" type="checkbox" bind:checked={showGrid} />
+	</div>
+	<svg width="100%" height="100%">
+		{#each polygons as polygon (polygon)}
+			<Polygon
+				id={polygon}
+				editing={polygon === activePolygon}
+				disabled={activePolygon !== undefined && polygon !== activePolygon}
+				editMode={activePolygon !== undefined}
+				{gridX}
+				{gridY}
+				on:closed={polygonClosed}
+			/>
+		{/each}
+	</svg>
+</div>
+
+<style>
+	.map {
+		background-image: url('/first-floor.png');
+		background-size: contain;
+		background-repeat: no-repeat;
+		width: 100%;
+		height: 800px;
+		position: absolute;
+	}
+
+	.grid {
+		pointer-events: none;
+		width: 100%;
+		height: 100%;
+		background-image: repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%),
+			repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%);
+		background-size: var(--grid-x) var(--grid-y);
+		position: absolute;
+	}
+
+	.controls {
+		position: absolute;
+		right: 0;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
