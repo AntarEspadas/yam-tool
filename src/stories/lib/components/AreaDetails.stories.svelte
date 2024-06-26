8e9ab968b207222a660962873d22b3d24152dcc2
Naratna <antar.espadas@hotmail.com>
2024-06-26 06:34:49 -0600

    Add stories for

---
+++ src/stories/lib/components/AreaDetails.stories.svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import AreaDetails from '$lib/components/AreaDetails.svelte';
+
+	export const meta: Meta<AreaDetails> = {
+		title: 'components/Area details',
+		component: AreaDetails,
+		tags: ['autodocs']
+	};
+</script>
+
+<Template let:args>
+	<AreaDetails
+		identifier="Lorem"
+		name="ipsum dolor sit amet"
+		description="Nesciunt illo quos perferendis voluptatibus odit. Nam sed commodi totam nulla, fugit voluptatem? Possimus cupiditate excepturi soluta earum nihil tempore voluptates saepe."
+		{...args}
+	/>
+</Template>
+
+<Story name="Default"></Story>
+
+<Story name="Empty description" args={{ description: '' }}></Story>
