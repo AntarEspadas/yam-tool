48636aedf20c51209ab7de9c63420a7d7ff6a4f4
Naratna <antar.espadas@hotmail.com>
2024-06-26 05:55:54 -0600

    Add stories for

---
+++ src/stories/lib/components/AreaDetailsEditor.stories.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import AreaDetailsEditor from '$lib/components/AreaDetailsEditor.svelte';
+
+	export const meta: Meta<AreaDetailsEditor> = {
+		title: 'components/Area details editor',
+		component: AreaDetailsEditor,
+		tags: ['autodocs']
+	};
+</script>
+
+<!-- <Template let:args>
+	<AreaDetailsEditor {...args} />
+</Template> -->
+
+<Story name="Default">
+	<AreaDetailsEditor
+		id="story-area"
+		identifier=""
+		name=""
+		description=""
+		forceDisableSubmit={false}
+		on:submit
+		on:delete
+	/>
+</Story>
