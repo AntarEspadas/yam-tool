f1a72b51db9be61ab1b4d4e57d8f5f48f93cdc20
Naratna <antar.espadas@hotmail.com>
2024-06-27 08:05:02 -0600

    Add sliders to

--- src/lib/components/GridSettings.svelte
+++ src/lib/components/GridSettings.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+	import { RangeSlider } from '@skeletonlabs/skeleton';
 
 	export let gridX: number;
 	export let gridY: number;
@@ -15,12 +16,16 @@
 		<svelte:fragment slot="content">
 			<label class="label"
 				><span>Grid X</span>
+				<br />
 				<input class="input" type="number" bind:value={gridX} />
 			</label>
+			<RangeSlider name="grid-x" bind:value={gridX} min={5} max={150} step={0.25}></RangeSlider>
 			<label class="label"
 				><span>Grid Y</span>
+				<br />
 				<input class="input" type="number" bind:value={gridY} />
 			</label>
+			<RangeSlider name="grid-y" bind:value={gridY} min={5} max={150} step={0.25}></RangeSlider>
 			<label>
 				<input class="checkbox" type="checkbox" bind:checked={showGrid} />
 				<span>Show grid</span>
@@ -28,3 +33,9 @@
 		</svelte:fragment>
 	</AccordionItem>
 </Accordion>
+
+<style>
+	.input {
+		width: 5.5em;
+	}
+</style>
