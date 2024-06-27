139ec5f2ae3b821a8e3a02a8f68850b64c301db8
Naratna <antar.espadas@hotmail.com>
2024-06-27 10:42:22 -0600

    Add dynamic aspect ratio to map image

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let gridX;
 	export let gridY;
 	export let showGrid: boolean;
 	export let src: string;
 
 	let ref: HTMLElement;
+
+	let mounted = false;
+
+	let aspectRatio = '1/1';
+
+	$: getSize(src, mounted);
+
+	onMount(() => {
+		mounted = true;
+	});
+
+	function getSize(src: string, mounted: boolean) {
+		if (!mounted) return;
+		const image = new Image();
+		image.onload = () => {
+			// Not a fan of mutating top level variables instead of returning values,
+			// but returning values from this callback would require the use of a promise,
+			// which can't be awaited at the top level, so this will do for now
+			aspectRatio = image.naturalWidth + '/' + image.naturalHeight;
+		};
+		image.src = src;
+	}
 </script>
 
 <div
@@ -12,6 +36,7 @@
 	class:grid={showGrid}
 	style="--grid-x:{gridX}px; --grid-y:{gridY}px"
 	style:--img="url('{src}')"
+	style:--aspect-ratio={aspectRatio}
 	bind:this={ref}
 >
 	<slot {ref} />
@@ -20,9 +45,9 @@
 <style>
 	.map {
 		background-image: var(--img);
-		background-size: cover;
+		background-size: contain;
 		background-repeat: no-repeat;
-		aspect-ratio: 1317/2141;
+		aspect-ratio: var(--aspect-ratio);
 		height: 800px;
 	}
 
--- src/stories/lib/components/Map.stories.svelte
+++ src/stories/lib/components/Map.stories.svelte
@@ -3,6 +3,7 @@
 	import type { Meta } from '@storybook/svelte';
 	import Map from '$lib/components/Map.svelte';
 	import image from './static/first-floor.png';
+	import image2 from './static/death-house.webp';
 
 	export const meta: Meta<Map> = {
 		title: 'components/Map',
@@ -12,8 +13,9 @@
 </script>
 
 <Template let:args>
-	<Map {...args} gridX={61.5} gridY={61.5} src={image} />
+	<Map gridX={61.5} gridY={61.5} src={image} showGrid={false} {...args} />
 </Template>
 
 <Story name="Grid on" args={{ showGrid: true }} />
-<Story name="Grid off" args={{ showGrid: false }} />
+<Story name="Grid off" />
+<Story name="Different image" args={{ src: image2 }} />
