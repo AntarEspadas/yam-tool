80895feda2880dae33c00eebf296a0ed5cd5a3c0
Naratna <antar.espadas@hotmail.com>
2024-06-27 10:20:18 -0600

    Improve grid styling

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -7,30 +7,31 @@
 	let ref: HTMLElement;
 </script>
 
-<div class="map" style:background-image="url('{src}')" bind:this={ref}>
-	<div style="width: 100%; height: 100%">
-		<slot {ref} />
-	</div>
-	{#if showGrid}
-		<div class="grid" style="--grid-x:{gridX}px; --grid-y:{gridY}px"></div>
-	{/if}
+<div
+	class="map"
+	class:grid={showGrid}
+	style="--grid-x:{gridX}px; --grid-y:{gridY}px"
+	style:--img="url('{src}')"
+	bind:this={ref}
+>
+	<slot {ref} />
 </div>
 
 <style>
-	.grid {
-		pointer-events: none;
-		width: 100%;
-		height: 100%;
-		background-image: repeating-linear-gradient(#ccc 0 1px, transparent 1px 100%),
-			repeating-linear-gradient(90deg, #ccc 0 1px, transparent 1px 100%);
-		background-size: var(--grid-x) var(--grid-y);
-		transform: translateY(-100%);
-	}
-
 	.map {
+		background-image: var(--img);
 		background-size: cover;
 		background-repeat: no-repeat;
 		aspect-ratio: 1317/2141;
 		height: 800px;
+	}
+
+	.map.grid {
+		background:
+			repeating-linear-gradient(#ccccccc2 0 2px, transparent 0px 100%) top / var(--grid-x)
+				var(--grid-y),
+			repeating-linear-gradient(90deg, #ccccccc2 0 2px, transparent 0px 100%) left / var(--grid-x)
+				var(--grid-y),
+			var(--img) left/cover no-repeat;
 	}
 </style>
