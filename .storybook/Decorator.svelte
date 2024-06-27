45b6cc45d4ed842fbf48b098f8844c9307bbb73c
Naratna <antar.espadas@hotmail.com>
2024-06-27 06:16:16 -0600

    Add material icons

--- .storybook/Decorator.svelte
+++ .storybook/Decorator.svelte
@@ -7,4 +7,11 @@
 	});
 </script>
 
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0"
+	/>
+</svelte:head>
+
 <slot />
