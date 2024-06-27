4228720cd0bf731b90e67267a375e310478490f4
Naratna <antar.espadas@hotmail.com>
2024-06-27 08:57:49 -0600

    Add story for

---
+++ src/stories/lib/components/Map.stories.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Map from '$lib/components/Map.svelte';
+	import image from './static/first-floor.png';
+
+	export const meta: Meta<Map> = {
+		title: 'components/Map',
+		component: Map,
+		tags: ['autodocs']
+	};
+</script>
+
+<Template let:args>
+	<Map {...args} gridX={61.5} gridY={61.5} src={image} />
+</Template>
+
+<Story name="Grid on" args={{ showGrid: true }} />
+<Story name="Grid off" args={{ showGrid: false }} />
