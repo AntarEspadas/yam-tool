95ad3a9d8d86fbbf28ef60d88bd9ffaac4c42bb3
Antar Espadas <antar.espadas@hotmail.com>
2024-06-30 01:02:59 -0600

    Add `Sidebar` component

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { Drawer } from 'vaul-svelte';
+	import { onMount } from 'svelte';
+
+	type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+	export let breakpoint: Breakpoint;
+	export let open = false;
+	export let direction: 'left' | 'right' = 'left';
+
+	let width = 0;
+	let isMounted = false;
+	let breakpointDiv: HTMLElement;
+
+	$: breakpointWidth = getWidth(breakpoint, isMounted);
+
+	onMount(() => {
+		window.addEventListener('resize', updateWidth);
+
+		width = window.innerWidth;
+
+		isMounted = true;
+
+		return () => {
+			window.removeEventListener('resize', updateWidth);
+		};
+	});
+
+	function updateWidth() {
+		width = window.innerWidth;
+	}
+
+	function getWidth(breakpoint: Breakpoint, mounted: boolean) {
+		if (!mounted) return Number.POSITIVE_INFINITY;
+		console.log('div', breakpointDiv);
+		const varValue = getComputedStyle(breakpointDiv).getPropertyValue('--' + breakpoint);
+		return Number(varValue?.replaceAll('px', ''));
+	}
+</script>
+
+<div class="breakpoints" bind:this={breakpointDiv}></div>
+{#if width >= breakpointWidth}
+	<slot />
+{:else}
+	<Drawer.Root bind:open shouldScaleBackground bind:direction>
+		<Drawer.Portal>
+			<Drawer.Overlay class="fixed inset-0 bg-black/40" />
+			<Drawer.Content class="sidebar-drawer-content {direction === 'left' ? 'left' : 'right'}">
+				<div class="card p-4">
+					<slot />
+				</div>
+			</Drawer.Content>
+		</Drawer.Portal>
+	</Drawer.Root>
+{/if}
+
+<style lang="scss">
+	.breakpoints {
+		display: none;
+		--xs: #{$xsBreakpoint};
+		--sm: #{$smBreakpoint};
+		--md: #{$mdBreakpoint};
+		--lg: #{$lgBreakpoint};
+		--xl: #{$xlBreakpoint};
+	}
+
+	.card {
+		height: 100%;
+	}
+
+	:global(.sidebar-drawer-content) {
+		position: fixed;
+		overflow-y: auto;
+		overflow-x: hidden;
+		top: 0;
+		bottom: 0;
+	}
+
+	:global(.sidebar-drawer-content.right) {
+		right: 0;
+		@include xs {
+			left: 25px;
+		}
+		@include sm {
+			left: auto;
+		}
+	}
+	:global(.sidebar-drawer-content.left) {
+		left: 0;
+		@include xs {
+			right: 25px;
+		}
+		@include sm {
+			right: auto;
+		}
+	}
+</style>
---
+++ src/stories/lib/components/Sidebar.stories.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+
+	export const meta: Meta<Sidebar> = {
+		title: 'components/Sidebar',
+		component: Sidebar,
+		tags: ['autodocs']
+	};
+</script>
+
+<Template let:args>
+	<Sidebar breakpoint="md" {...args}>
+		<ul class="ul" style="width: 300px; height: 100%; overflow-y: auto">
+			{#each new Array(15).fill(0) as _, i}
+				<li>
+					<div class="card p-4">
+						<p>Element {i + 1}</p>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</Sidebar>
+</Template>
+
+<Story name="Left - hidden" args={{ direction: 'left' }}></Story>
+<Story name="Right - hidden" args={{ direction: 'right' }}></Story>
+<Story name="Left - visible" args={{ direction: 'left', open: true, breakpoint: 'xl' }}></Story>
+<Story name="Right - visible" args={{ direction: 'right', open: true, breakpoint: 'xl' }}></Story>
