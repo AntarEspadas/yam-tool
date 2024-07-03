95dcea459118bf304cf69fd3e2ce9c8063660bc4
Antar Espadas <antar.espadas@hotmail.com>
2024-07-03 12:37:21 -0600

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { page } from "$app/stores"
+</script>
+
+<div>
+  <h1 class="text-surface-700-200-token h1">
+    {$page.error?.message}
+  </h1>
+  <h2 class="text-surface-700-200-token h2">
+    {$page.status}
+  </h2>
+  <a href="/maps">
+    <button class="variant-soft-primary btn">Back to maps</button>
+  </a>
+</div>
+
+<style>
+  div {
+    text-align: center;
+  }
+  h2 {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+</style>
