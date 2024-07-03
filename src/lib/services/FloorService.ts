7a9bcaed302fd3986bff581effa2a7e495965eb1
Antar Espadas <antar.espadas@hotmail.com>
2024-07-03 11:03:33 -0600

    Fix implementation of floor and map deletion

--- src/lib/services/FloorService.ts
+++ src/lib/services/FloorService.ts
@@ -1,9 +1,16 @@
 import type { Floor } from "$lib/types"
 import { type MapDb, db } from "$lib/db/indext"
+import { areaService, type AreaService } from "./AreaService"
+import { polygonService, type PolygonService } from "./PolygonService"
+import { Dexie } from "dexie"
 export class FloorService {
   private static readonly KEY = "floors"
 
-  constructor(private db: MapDb) {}
+  constructor(
+    private db: MapDb,
+    private areaService: AreaService,
+    private polygonService: PolygonService
+  ) {}
 
   public addFloor(mapId: number) {
     return this.db.floors.add({
@@ -32,9 +39,19 @@ export class FloorService {
     return this.db.floors.where("mapId").equals(mapId).sortBy("sortOrder")
   }
 
-  public deleteFloorById(floorId: number) {
-    return this.db.floors.delete(floorId)
+  public async deleteFloorById(floorId: number) {
+    return this.db.transaction("rw", this.db.floors, this.db.areas, this.db.polygons, async () => {
+      const [areas, polygons] = await Dexie.Promise.all([
+        this.areaService.getAreasByFloor(floorId),
+        this.polygonService.getPolygonsByFloor(floorId),
+      ])
+      return Dexie.Promise.all([
+        this.db.areas.bulkDelete(areas.map((a) => a.id)),
+        this.db.polygons.bulkDelete(polygons.map((p) => p.id)),
+        this.db.floors.delete(floorId),
+      ])
+    })
   }
 }
 
-export const floorService = new FloorService(db)
+export const floorService = new FloorService(db, areaService, polygonService)
--- src/lib/services/MapService.ts
+++ src/lib/services/MapService.ts
@@ -1,9 +1,14 @@
 import type { MapDb } from "$lib/db/indext"
 import type { Map } from "$lib/types"
 import { db } from "$lib/db/indext"
+import Dexie from "dexie"
+import { floorService, type FloorService } from "./FloorService"
 
 export class MapService {
-  constructor(private db: MapDb) {}
+  constructor(
+    private db: MapDb,
+    private floorService: FloorService
+  ) {}
 
   public getMapById(mapId: number) {
     return this.db.maps.where("id").equals(mapId).first()
@@ -21,7 +26,33 @@ export class MapService {
   }
 
   public deleteMapById(mapId: number) {
-    return this.db.maps.delete(mapId)
+    return this.db.transaction(
+      "rw",
+      this.db.maps,
+      this.db.floors,
+      this.db.areas,
+      this.db.polygons,
+      async () => {
+        const floors = await this.floorService.getFloorsByMapId(mapId)
+        const floorIds = floors.map((f) => f.id)
+        const [areas, polygons] = await Dexie.Promise.all([
+          this.db.areas
+            .where("floorId")
+            .anyOf(...floorIds)
+            .toArray(),
+          this.db.polygons
+            .where("floorId")
+            .anyOf(...floorIds)
+            .toArray(),
+        ])
+        return Dexie.Promise.all([
+          this.db.maps.delete(mapId),
+          this.db.floors.bulkDelete(floorIds),
+          this.db.areas.bulkDelete(areas.map((a) => a.id)),
+          this.db.polygons.bulkDelete(polygons.map((p) => p.id)),
+        ])
+      }
+    )
   }
 
   public getMaps() {
@@ -37,4 +68,4 @@ const newMapDescription = `## Welcome to your new map
 - You can edit your map's title, description and preview image by clicking the \`edit\` button below
 - You can add styles like **bold** and *italic* to the description with [Markdown](https://www.markdownguide.org/basic-syntax/) syntax`
 
-export const mapService = new MapService(db)
+export const mapService = new MapService(db, floorService)
